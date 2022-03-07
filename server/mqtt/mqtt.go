1b570feb226ab131cf335b9340004c066856ef5e
深蓝 <mail@ri-co.cn>
2022-03-07 14:57:41 +0800

    优化mqtt结构

--- server/mqtt/mqtt.go
+++ server/mqtt/mqtt.go
@@ -1,7 +1,6 @@
 package mqtt
 
 import (
-	"encoding/json"
 	"fmt"
 	"time"
 
@@ -57,75 +56,4 @@ func sub() {
 	topic := "iot1/server/res"
 	token := MqClient.Subscribe(topic, 2, msgHandler)
 	token.Wait()
-}
-
-//SendMqtt 通过mqtt发送JSON。topic首位不需要添加 '/'
-func SendMqttJson(topic string, payload interface{}) {
-	data, err := json.Marshal(payload)
-	if err != nil {
-		log.Log.Warnln("server-mqtt 发送json 格式化出错", err.Error())
-		return
-	}
-	go func() {
-		token := MqClient.Publish(Topic+topic, 2, false, data)
-		err = token.Error()
-		if err != nil {
-			log.Log.Warnln("server-mqtt 发送json 失败", err.Error())
-			return
-		}
-		token.Wait()
-	}()
-
-}
-
-//SendMqttString 通过mqtt发送消息，文本格式
-func SendMqttString(topic, payload string) {
-	go func() {
-		token := MqClient.Publish(Topic+topic, 2, false, payload)
-		err := token.Error()
-		if err != nil {
-			log.Log.Warnln("server-mqtt 发送文本出错", err.Error())
-			return
-		}
-		token.Wait()
-	}()
-}
-
-//SendMqttInfo 通过mqtt发送info调试消息，文本格式
-func SendMqttInfo(payload string) {
-	go func() {
-		token := MqClient.Publish(Topic+"log/master/info", 2, false, payload)
-		err := token.Error()
-		if err != nil {
-			log.Log.Warnln("server-mqtt 发送info失败", err.Error())
-			return
-		}
-		token.Wait()
-	}()
-}
-
-//SendMqttWarn 通过mqtt发送warn信息，文本格式
-func SendMqttWarn(payload string) {
-	go func() {
-		token := MqClient.Publish(Topic+"log/master/warn", 2, false, payload)
-		err := token.Error()
-		if err != nil {
-			log.Log.Warnln("server-mqtt 发送info失败", err.Error())
-			return
-		}
-		token.Wait()
-	}()
-}
-
-//SendMqttError 通过mqtt发送error信息，文本格式
-func SendMqttError(payload string) {
-	go func() {
-		token := MqClient.Publish(Topic+"log/master/error", 2, false, payload)
-		err := token.Error()
-		if err != nil {
-			log.Log.Warnln("server-mqtt 发送info失败", err.Error())
-			return
-		}
-		token.Wait()
-	}()
 }
