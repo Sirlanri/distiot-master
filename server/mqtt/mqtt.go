15168e83b21b52137c9e220aa426e77fc25bd6b8
深蓝 <mail@ri-co.cn>
2022-03-07 14:10:40 +0800

    mqtt包 完成

--- server/mqtt/mqtt.go
+++ server/mqtt/mqtt.go
@@ -20,7 +20,11 @@ var msgHandler = func(client mqtt.Client, msg mqtt.Message) {
 	log.Log.Debugln("接收到指令：", string(msg.Payload()))
 }
 
-var c mqtt.Client
+//全局MQ客户端 通过此发送接收消息
+var MqClient mqtt.Client
+
+//全局topic distiot/
+const Topic = "distiot/"
 
 //Createid 生成唯一名称
 func Createid() string {
@@ -40,8 +44,8 @@ func init() {
 	opts.SetDefaultPublishHandler(f)
 	opts.SetPingTimeout(1 * time.Second)
 
-	c = mqtt.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
+	MqClient = mqtt.NewClient(opts)
+	if token := MqClient.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 	sub()
@@ -51,22 +55,23 @@ func init() {
 //sub 订阅某个主题的消息
 func sub() {
 	topic := "iot1/server/res"
-	token := c.Subscribe(topic, 2, msgHandler)
+	token := MqClient.Subscribe(topic, 2, msgHandler)
 	token.Wait()
-
 }
 
-//SendMqtt 通过mqtt发送消息
-func SendMqtt(payload interface{}) {
+//SendMqtt 通过mqtt发送JSON。topic首位不需要添加 '/'
+func SendMqttJson(topic string, payload interface{}) {
 	data, err := json.Marshal(payload)
 	if err != nil {
-		log.Log.Errorln("json打包出错", err.Error())
+		log.Log.Warnln("server-mqtt 发送json 格式化出错", err.Error())
+		return
 	}
 	go func() {
-		token := c.Publish("iot1/server/send", 2, false, data)
+		token := MqClient.Publish(Topic+topic, 2, false, data)
 		err = token.Error()
 		if err != nil {
-			log.Log.Errorln("mqtt出错", err.Error())
+			log.Log.Warnln("server-mqtt 发送json 失败", err.Error())
+			return
 		}
 		token.Wait()
 	}()
@@ -74,24 +79,52 @@ func SendMqtt(payload interface{}) {
 }
 
 //SendMqttString 通过mqtt发送消息，文本格式
-func SendMqttString(payload string) {
+func SendMqttString(topic, payload string) {
 	go func() {
-		token := c.Publish("iot1/server/send", 2, false, payload)
+		token := MqClient.Publish(Topic+topic, 2, false, payload)
 		err := token.Error()
 		if err != nil {
-			log.Log.Errorln("mqtt出错", err.Error())
+			log.Log.Warnln("server-mqtt 发送文本出错", err.Error())
+			return
 		}
 		token.Wait()
 	}()
 }
 
-//SendMqttInfo 通过mqtt发送报错/消息，文本格式
+//SendMqttInfo 通过mqtt发送info调试消息，文本格式
 func SendMqttInfo(payload string) {
 	go func() {
-		token := c.Publish("iot1/server/info", 2, false, payload)
+		token := MqClient.Publish(Topic+"log/master/info", 2, false, payload)
 		err := token.Error()
 		if err != nil {
-			log.Log.Errorln("mqtt出错", err.Error())
+			log.Log.Warnln("server-mqtt 发送info失败", err.Error())
+			return
+		}
+		token.Wait()
+	}()
+}
+
+//SendMqttWarn 通过mqtt发送warn信息，文本格式
+func SendMqttWarn(payload string) {
+	go func() {
+		token := MqClient.Publish(Topic+"log/master/warn", 2, false, payload)
+		err := token.Error()
+		if err != nil {
+			log.Log.Warnln("server-mqtt 发送info失败", err.Error())
+			return
+		}
+		token.Wait()
+	}()
+}
+
+//SendMqttError 通过mqtt发送error信息，文本格式
+func SendMqttError(payload string) {
+	go func() {
+		token := MqClient.Publish(Topic+"log/master/error", 2, false, payload)
+		err := token.Error()
+		if err != nil {
+			log.Log.Warnln("server-mqtt 发送info失败", err.Error())
+			return
 		}
 		token.Wait()
 	}()
