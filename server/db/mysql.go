fcac01ff2aed6d87ae465cb53e4ce44720fa4e3b
深蓝 <mail@ri-co.cn>
2022-03-08 08:59:24 +0800

    新增gorm连接数据库

--- server/db/mysql.go
+++ server/db/mysql.go
@@ -3,12 +3,15 @@ package db
 import (
 	"github.com/Sirlanri/distiot-master/server/log"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"github.com/jmoiron/sqlx"
 )
 
 var (
 	//全局DB指针
 	db *sqlx.DB
+	//MySQL数据库指针
+	Mdb *gorm.DB
 )
 
 func init() {
@@ -30,5 +33,21 @@ func connectMysql() {
 		return
 	}
 	log.Log.Infoln("server- MySQL链接完成")
+
+}
+
+func connectMysqlByGorm() {
+	var err error
+	Mdb, err = gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/distiot")
+	if err != nil {
+		log.Log.Errorln("server-db MySQL连接失败")
+		return
+	}
+	err = Mdb.DB().Ping()
+	if err != nil {
+		log.Log.Errorln("server-db MySQL ping失败")
+		return
+	}
+	log.Log.Infoln("server-db MySQL连接成功")
 
 }
