705f8cbe44745c3e0665dd03244a1c6ba33158d7
深蓝 <mail@ri-co.cn>
2022-03-09 09:25:15 +0800

    优化文件结构

---
+++ node/nodeData.go
@@ -0,0 +1,58 @@
+package node
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/Sirlanri/distiot-master/server/db"
+	"github.com/Sirlanri/distiot-master/server/log"
+)
+
+//根据node的id，在Redis获取该node的地址、端口信息
+func FindNodeRds(id string) (addr string, port string, err error) {
+	cmd := db.Rdb.LRange(db.RedisCtx, id, 0, 1)
+	value, err := cmd.Result()
+	if err != nil || len(value) == 0 {
+		//log.Log.Warnln("node- FindNodeRds 无法获取该node信息:", id)
+		err = errors.New("node- FindNodeRds 无法获取该node信息: " + id)
+		return "", "0", err
+	}
+	return value[0], value[1], nil
+}
+
+//根据node的id，在MySQL中获取该node的地址、端口信息
+func FindNodeMysql(id string) (node db.Node, err error) {
+	db.Mdb.First(&node, id)
+	if node.Addr == "" {
+		log.Log.Warnln("node MySQL查找该节点信息失败:", id)
+		return node, errors.New("node MySQL查找该节点信息失败")
+	}
+	log.Log.Debugln("MySQL查找成功 ", node)
+	return node, nil
+}
+
+//将nodes的单个数据写入redis
+func InsertNodeRedis(node *db.Node) error {
+	_, err := db.Rdb.RPush(db.RedisCtx, strconv.Itoa(node.Id),
+		node.Addr, node.Port).Result()
+	if err != nil {
+		log.Log.Warnln("node- InsertNodeRedis node节点信息写入Redis失败 ", err.Error())
+		return errors.New("node- InsertNodeRedis node节点信息写入Redis失败 ")
+	}
+	return nil
+}
+
+//将node的单个数据写入MySQL
+func InsertNodeMysql(addr string, port int) error {
+	res := db.Mdb.Create(&db.Node{
+		Id:   0,
+		Addr: addr,
+		Port: port,
+	})
+	if res.Error != nil {
+		log.Log.Warnln("node- InsertNodeMysql 插入数据失败 ", res.Error)
+		return res.Error
+	}
+	log.Log.Debugln("写入成功", res.RowsAffected)
+	return nil
+}
