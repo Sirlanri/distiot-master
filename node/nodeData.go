27b7d60dbb5b4cdcb5efb0d08d1e40c4b0c23019
深蓝 <mail@ri-co.cn>
2022-03-18 22:09:31 +0800

    更改id为int

--- node/nodeData.go
+++ node/nodeData.go
@@ -9,20 +9,20 @@ import (
 )
 
 //根据node的id，在Redis获取该node的地址、端口信息
-func FindNodeRds(id string) (addr string, port string, err error) {
-	id = "nodeinfo" + id
-	cmd := db.Rdb.LRange(db.RedisCtx, id, 0, 1)
+func FindNodeRds(id int) (addr string, port string, err error) {
+	ids := "nodeinfo" + strconv.Itoa(id)
+	cmd := db.Rdb.LRange(db.RedisCtx, ids, 0, 1)
 	value, err := cmd.Result()
 	if err != nil || len(value) == 0 {
 		//log.Log.Warnln("node- FindNodeRds 无法获取该node信息:", id)
-		err = errors.New("node- FindNodeRds 无法获取该node信息: " + id)
+		err = errors.New("node- FindNodeRds 无法获取该node信息: " + ids)
 		return "", "0", err
 	}
 	return value[0], value[1], nil
 }
 
 //根据node的id，在MySQL中获取该node的地址、端口信息
-func FindNodeMysql(id string) (node db.Node, err error) {
+func FindNodeMysql(id int) (node db.Node, err error) {
 	db.Mdb.First(&node, id)
 	if node.Addr == "" {
 		log.Log.Warnln("node MySQL查找该节点信息失败:", id)
@@ -44,6 +44,32 @@ func InsertNodeRedis(node *db.Node) error {
 	return nil
 }
 
+//更新Redis中的单个nodeinfo数据，有则更新，无则添加
+func UpdateNodeRedis(node *db.Node) error {
+	_, _, err := FindNodeRds(node.Id)
+	if err != nil {
+		//找不到 不存在 插入节点
+		InsertNodeRedis(node)
+		return nil
+	}
+	//node已存在，更新Redis信息
+	key := "nodeinfo" + strconv.Itoa(node.Id)
+	_, err = db.Rdb.LSet(db.RedisCtx, key,
+		0, node.Addr).Result()
+	if err != nil {
+		log.Log.Warnln("node- DelNodeRedis addr信息更新Redis失败 ", err.Error())
+		return err
+	}
+	_, err = db.Rdb.LSet(db.RedisCtx, key,
+		1, node.Port).Result()
+	if err != nil {
+		log.Log.Warnln("node- DelNodeRedis port信息更新Redis失败 ", err.Error())
+		return err
+	}
+	return nil
+
+}
+
 //将node的单个数据写入MySQL，返回此node的id （未来优化性能入手点之一）
 func InsertNodeMysql(addr string, port int) (int, error) {
 	node := &db.Node{
@@ -58,4 +84,14 @@ func InsertNodeMysql(addr string, port int) (int, error) {
 	}
 	log.Log.Debugln("写入成功", res.RowsAffected)
 	return node.Id, nil
+}
+
+//更新已有node的信息至MySQL
+func UpdateNodeMysql(node *db.Node) error {
+	res := db.Mdb.Save(&node)
+	if res.Error != nil {
+		log.Log.Warnln("node- UpdateNodeMysql 更新数据失败 ", res.Error)
+		return res.Error
+	}
+	return nil
 }
