91639ef0acc8b4a529bff386e39f86c01e341710
深蓝 <mail@ri-co.cn>
2022-04-06 15:35:53 +0800

    删除main的测试代码

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/Sirlanri/distiot-master/device"
 	_ "github.com/Sirlanri/distiot-master/node"
 	_ "github.com/Sirlanri/distiot-master/server/db"
 	"github.com/Sirlanri/distiot-master/server/log"
@@ -14,15 +13,6 @@ func main() {
 	var wg2 sync.WaitGroup
 	wg2.Add(1)
 	start := time.Now()
-	node := device.FindNodeNow(2)
-	log.Log.Debugln("：1 不需要插入", node)
-	node = device.FindNodeNow(3)
-	log.Log.Debugln("：1 插入redis 3", node)
-	node = device.FindNodeNow(1)
-	log.Log.Debugln("：2 插入redis 1", node)
-
-	node = device.FindNodeNow(4)
-	log.Log.Debugln("随机 1 2", node)
 
 	elapsed := time.Since(start)
 	log.Log.Debugln("执行时间 ", elapsed)
