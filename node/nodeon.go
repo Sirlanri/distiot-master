91c47a071d04613d1291c610bd110a0bf551fe93
深蓝 <mail@ri-co.cn>
2022-04-13 10:26:33 +0800

    新增 分配nodeID

--- node/nodeon.go
+++ node/nodeon.go
@@ -3,6 +3,8 @@ package node
 
 import (
 	"errors"
+	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/Sirlanri/distiot-master/server/log"
@@ -28,6 +30,20 @@ func findNodeByid(id int) (addr, port string, err error) {
 //为新node分配id，将id发送至node节点
 func DistributeID(id int, addr string, port int) {
 	log.Log.Infoln("分配ID ", id, addr, port)
+	params := url.Values{}
+	Url, err := url.Parse(addr + ":" + strconv.Itoa(port) + "/node/allocateid")
+	if err != nil {
+		log.Log.Warnln("node- DistributeID url解析失败 ", err.Error())
+		return
+	}
+	params.Set("id", strconv.Itoa(id))
+	Url.RawQuery = params.Encode()
+	res, err := http.Get(Url.String())
+	if err != nil {
+		log.Log.Warnln("node- DistributeID http get失败 ", err.Error())
+		return
+	}
+	log.Log.Infoln("node- DistributeID 返回结果：", res.StatusCode)
 }
 
 //确认节点上线 发送至node
